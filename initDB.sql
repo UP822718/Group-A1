6bd4d5c879a87d60f98c0c706f687f7019a5e0b4
Stephen <stephenvaneemail@gmail.com>
2019-03-07 20:39:04 +0000

    initDB + Main.js Basic Routes
    Added a SQL file called 'initDB'. This is run to setup the local database for testing. Main.js now connects to the login database and has post routes for login and signup.

---
+++ initDB.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE IF NOT EXISTS login;
+
+CREATE TABLE IF NOT EXISTS login.user (
+  userID int auto_increment primary key,
+  username varchar(15) not null,
+  password varchar(60) not null;
+)
--- server/main.js
+++ server/main.js
@@ -1,1 +1,39 @@
 "use strict";
+
+const express = require('express');
+const mysql = require('mysql2');
+/*
+const googleAuth = require('simple-google-openid');
+const bcrypt = require('bcrypt');
+*/
+
+const app = express();
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "login"
+});
+
+connection.connect(function(e) {
+    if (e) {
+        throw e;
+    }
+    else {
+       console.log("Connection to database established..");
+    }
+});
+
+app.use(express.urlencoded());
+app.use(express.static('./static'));
+
+app.post('/login', authLogin);
+app.post('/signup', authUser);
+
+async function authLogin(req,res) {
+  
+}
+async function authUser(req,res) {
+
+}
+app.listen(8080, console.log("Listening.."));
