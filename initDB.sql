08d7dfc7641ff17036681ea3f78cdbf9d8b2d275
up822718 <up822718@myport.ac.uk>
2019-03-09 22:00:38 +0000

    Update initDB.sql
    fix number two

--- initDB.sql
+++ initDB.sql
@@ -3,5 +3,5 @@ CREATE DATABASE IF NOT EXISTS login;
 CREATE TABLE IF NOT EXISTS login.user (
   userID int auto_increment primary key,
   username varchar(15) not null,
-  password varchar(60) not null;
+  password varchar(60) not null
 );
