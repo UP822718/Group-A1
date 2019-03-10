1feb66e645ecd6ee3912eae8b1b240fa4923be23
up822718 <up822718@myport.ac.uk>
2019-03-10 12:09:02 +0000

    Update initDB.sql

--- initDB.sql
+++ initDB.sql
@@ -4,5 +4,4 @@ CREATE TABLE IF NOT EXISTS login.login (
   userID INT NOT NULL AUTO_INCREMENT,
   username VARCHAR(15) NULL,
   password VARCHAR(60) NULL,
-  PRIMARY KEY (`userID`))
-ENGINE = InnoDB
+  PRIMARY KEY (userID));
