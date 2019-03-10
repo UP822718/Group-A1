ec1c51baef32f28792a71a0502ea3e3efe74b51e
up822718 <up822718@myport.ac.uk>
2019-03-10 12:07:07 +0000

    Update initDB.sql
    rmeove ' form sql code

--- initDB.sql
+++ initDB.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE IF NOT EXISTS login;
 
-CREATE TABLE IF NOT EXISTS `login`.`login` (
-  `userID` INT NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(15) NULL,
-  `password` VARCHAR(60) NULL,
+CREATE TABLE IF NOT EXISTS login.login (
+  userID INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(15) NULL,
+  password VARCHAR(60) NULL,
   PRIMARY KEY (`userID`))
 ENGINE = InnoDB
