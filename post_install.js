377ce5ff9c5267209d66ca2b7d0ffbf6d7f23294
UP822718 <36608060+UP822718@users.noreply.github.com>
2019-03-13 15:51:35 +0000

    SQL password fix

--- post_install.js
+++ post_install.js
@@ -4,7 +4,7 @@ let fs = require('fs');
 let initDBsql = require('./initDBsql.json');
 
 async function sqlSetup() {
-  const connection = await mysql2.createConnection({ host: "localhost",user: "root",password: "root"});
+  const connection = await mysql2.createConnection({ host: "localhost",user: "root",password: ""});
 
     for (let varsql of initDBsql.sql) {
      await connection.execute(varsql );
