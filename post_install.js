ecb32bf37f3827b11e52303adb73a9a1ab794022
UP822718 <36608060+UP822718@users.noreply.github.com>
2019-03-13 14:46:54 +0000

    post_install.js neely working
    code not working but alsomst the post_install.js

--- post_install.js
+++ post_install.js
@@ -1,10 +1,13 @@
+"use strict"
 let mysql2 = require('mysql2')
 let fs = require('fs');
-async function  sql_init() {
-  const sqlSetup = fs.readFileSync('initDB.sql').toString();
-  const connection = await mysql2.createConnection({
-    host: "localhost",
-    user: "root"});
-  connection.execute(sqlSetup);
+let initDBsql = require('./initDBsql.json');
+
+async function sqlSetup() {
+  const connection = await mysql2.createConnection({ host: "localhost",user: "root",password: "root"});
+
+    for (varsql of initDBsql.sql) {
+     await connection.execute(variable);
+    }
 }
-sql_init();
+sqlSetup();
