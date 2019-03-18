723ab4fc52b15d061e0ca0823f3a8426b1100764
William <up822718@myport.ac.uk>
2019-03-18 19:29:22 +0000

    fix up some css

---
+++ static/js/api.js
@@ -0,0 +1,40 @@
+function setFirstName(name) {
+await fetch('/api/firstname', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/text',
+      'Content-Type': 'application/text'
+    },
+    body: name
+  });
+}
+
+function setLastName() {
+  fetch('');
+
+}
+
+function setLastName() {
+  fetch('');
+
+}
+
+function setWeight() {
+  fetch('');
+
+}
+
+function setHeight() {
+  fetch('');
+
+}
+
+function setHydration() {
+  fetch('');
+
+}
+
+function setStepsTaken() {
+  fetch('');
+
+}
