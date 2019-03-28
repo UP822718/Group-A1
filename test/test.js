cbe20c16395084a017a88594bcf2b06164b7cdd8
UP822718 <36608060+UP822718@users.noreply.github.com>
2019-03-28 20:23:39 +0000

    Update test.js
    adding some signup test

--- test/test.js
+++ test/test.js
@@ -5,3 +5,10 @@ const puppeteer = require('puppeteer');
 const browser = await puppeteer.launch();
 const page = await browser.newPage();
 await page.goto('http://localhost:8080');
+
+describe('signup', function () {
+  it('it shuld allow me to make a user with the password set to test and the user name set to test when there are no user in the DB',function() {
+
+  })
+  it('it sguld not allow me to put in to password that are diffrunt')
+});
