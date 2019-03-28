8cf91e5563d651ef5d21f1c876955b823d065fea
UP822718 <36608060+UP822718@users.noreply.github.com>
2019-03-28 20:18:37 +0000

    got web boswer ready for testing

--- test/test.js
+++ test/test.js
@@ -1,1 +1,7 @@
-let main = require('../server/main.js');
+const main = require('../server/main.js');
+const assert = require('assert');
+const puppeteer = require('puppeteer');
+
+const browser = await puppeteer.launch();
+const page = await browser.newPage();
+await page.goto('http://localhost:8080');
