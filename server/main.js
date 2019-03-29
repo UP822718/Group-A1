833bb450005c987f594e2fdd036a3e3f662064bc
William <up822718@myport.ac.uk>
2019-03-29 23:17:21 +0000

    update

--- server/main.js
+++ server/main.js
@@ -116,11 +116,11 @@ async function authLogin(req, res) {
     let sql = 'SELECT * FROM users WHERE username = ?';
     try {
 
-      const [rows_userCheck, fields_userCheck, ] = await connection.query(sql, username);
+      const [rows, fields] = await connection.execute(sql, username);
     } catch (err) {
 
     }
-    if (rows_userCheck.length > 0) {
+    if (rows.length > 0) {
       bcrypt.compare(req.body.password, rows_userCheck[0].password, function(e, result) {
         if (result) {
           console.log("Password Matches");
