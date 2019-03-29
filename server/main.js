2529d9927f0d5e6173046040bd3a9b12b66af080
William <up822718@myport.ac.uk>
2019-03-29 20:38:09 +0000

    upladte

--- server/main.js
+++ server/main.js
@@ -3,25 +3,42 @@ const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const mysql = require('mysql2/promise');
-
-
 const bcrypt = require('bcrypt');
 
 const app = express();
+let connection ={}
 
-let connection = {}
-init();
 async function init() {
-  console.log("test");
-connection = await mysql.createConnection( { host: "localhost",user: "root",password: "root" ,database: "fitnessprofile"} );
+   connection= await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "fitnessprofile"
+  });
 }
 
-app.use(express.static('views'));
-app.use(bodyParser.urlencoded({extend:true}));
+init();
+/**
+ * connection - description
+ *
+ * @param  {type} function(e description
+ * @return {type}            description
+ */
+// connection.connect(function(e) {
+//   if (e) {
+//     throw e;
+//   } else {
+//     console.log("Connection to database established..");
+//   }
+// });
+// app.use(express.static('views'));
+app.use(bodyParser.urlencoded({
+  extend: true
+}));
 app.use(session({
-    secret: 'temp-secret',
-    resave: false,
-    saveUninitialized: false
+  secret: 'temp-secret',
+  resave: false,
+  saveUninitialized: false
 }));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
@@ -33,9 +50,11 @@ app.set('view engine', 'html');
  * @param  {type} res          description
  * @return {type}              description
  */
-app.get('/', function(req,res) {
-    response.render('index');
+app.get('/', function(req, res) {
+  res.render('index');
 });
+app.use('/css', express.static('views/css'))
+app.use('/js', express.static('views/js'))
 
 /**
  * app - description
@@ -45,61 +64,31 @@ app.get('/', function(req,res) {
  * @param  {type} res          description
  * @return {type}              description
  */
-app.get('/profile', function(req,res) {
-    /* Authentication */
-    if (!req.session || !req.session.authenticate) {
-        res.sendStatus(401); // not authorized status
-    }
-    else {
-        /* if authenticated */
-        //let statsArray = [];
+app.get('/profile', async function(req, res) {
+  /* Authentication */
+  if (!req.session || !req.session.authenticate) {
+    res.sendStatus(401); // not authorized status
+  } else {
+    /* if authenticated */
+    //let statsArray = [];
 
-        console.log("Showing profile for users", req.session.username);
-        /* get all values from DB*/
-        console.log(req.session.userID);
-        /* GET HYDRATION */
-        let sqlHydration = 'SELECT hydrationValue FROM hydration WHERE userID = ?';
-        connection.query(sqlHydration, req.session.userID, function(e, results) {
-           if (e) {
-             throw e;
-           }
-           else {
-             /*hydration = results[0].hydrationValue;*/
-             res.render('Statistics_Page', {hydration: results[0].hydrationValue});
-           }
-         });
-         /*
-         let sqlWeight = 'SELECT weightValue FROM weight WHERE userID = ?';
-         connection.query(sqlWeight, req.session.userID, function(e, results) {
-            if (e) {
-              throw e;
-            }
-            else {
-              statsArray.push(results[0].weightValue);
-            }
-          });
-          let sqlCalories = 'SELECT caloriesValue FROM calories WHERE userID = ?';
-          connection.query(sqlCalories, req.session.userID, function(e, results) {
-             if (e) {
-               throw e;
-             }
-             else {
-               statsArray.push(results[0].caloriesValue);
-             }
-           });
-           let sqlSteps = 'SELECT stepsValue FROM steps WHERE userID = ?';
-           connection.query(sqlSteps, req.session.userID, function(e, results) {
-              if (e) {
-                throw e;
-              }
-              else {
-                statsArray.push(results[0].stepsValue);
-              }
-            });
-
-          res.render('Statistics_Page', {hydration: hydration, weight: weight, calories: calories, steps: steps});
-          */
-    }
+    console.log("Showing profile for users", req.session.username);
+    /* get all values from DB*/
+    console.log(req.session.userID);
+    /* GET HYDRATION */
+    let sqlHydration = 'SELECT hydrationValue FROM hydration WHERE userID = ?';
+    const [rows_hydration, fields_hydration] = await connection.execute(sqlHydration, req.session.userID);
+    console.log(rows_hydration);
+    let sqlWeight = 'SELECT weightValue FROM weight WHERE userID = ?';
+    console.log(rows_Weight);
+    let sqlCalories = 'SELECT caloriesValue FROM calories WHERE userID = ?';
+    const [rows_Calories, fields_Calories] = await connection.execute(sqlCalories, req.session.userID);
+    console.log(rows_Calories);
+    let sqlSteps = 'SELECT stepsValue FROM steps WHERE userID = ?';
+    const [rows_Steps, fields_Steps] = await connection.execute(sqlSteps, req.session.userID);
+    console.log(rows_Steps);
+    res.render('Statistics_Page', {});
+  }
 });
 
 app.post('/login', authLogin);
@@ -115,41 +104,37 @@ app.post('/add', addStat);
  * @param  {type} res description
  * @return {type}     description
  */
-async function authLogin(req,res) {
-    const username = req.body.username;
-    let sql = 'SELECT * FROM users WHERE username = ?';
+async function authLogin(req, res) {
+  const username = req.body.username;
+  let sql = 'SELECT * FROM users WHERE username = ?';
 
-    connection.query(sql, username, function(e, results) {
-       if (e) {
-         throw e;
-       }
-       else {
-         if (results.length > 0) {
-           bcrypt.compare(req.body.password, results[0].password, function(e,result) {
-             if (result) {
-                console.log("Password Matches");
-                req.session.authenticate = true;
-                req.session.userID = results[0].userID;
-                req.session.username = username;
-                res.redirect("/profile");
+  connection.query(sql, username, function(e, results) {
+    if (e) {
+      throw e;
+    } else {
+      if (results.length > 0) {
+        bcrypt.compare(req.body.password, results[0].password, function(e, result) {
+          if (result) {
+            console.log("Password Matches");
+            req.session.authenticate = true;
+            req.session.userID = results[0].userID;
+            req.session.username = username;
+            res.redirect("/profile");
 
-             }
-             else {
-                console.log("Password Doesn't Match");
-                res.redirect("/");
-             }
-           });
-         }
-         else {
-           console.log("Username Doesnt match any Accounts");
-           res.redirect("/");
-         }
-       }
-     });
+          } else {
+            console.log("Password Doesn't Match");
+            res.redirect("/");
+          }
+        });
+      } else {
+        console.log("Username Doesnt match any Accounts");
+        res.redirect("/");
+      }
+    }
+  });
 }
 
-async function addStat(req,res) {
-}
+async function addStat(req, res) {}
 /**
  * authUser - description
  *
@@ -157,36 +142,33 @@ async function addStat(req,res) {
  * @param  {type} res description
  * @return {type}     description
  */
-async function authUser(req,res) {
-    const username = req.body.username;
-    let sql = 'SELECT * FROM users WHERE username = ?';
+async function authUser(req, res) {
+  const username = req.body.username;
+  let sql = 'SELECT * FROM users WHERE username = ?';
 
-    connection.query(sql, username, function(e, results) {
-       if (e) {
-         throw e;
-       }
-       else {
-          if (results.length > 0) {
-             console.log("Username Already Exists");
-             res.redirect("signup");
-          }
-          else {
-             bcrypt.hash(req.body.password, 10, function(e, hash) {
-                 let sql = 'INSERT INTO users (username,password) VALUES (?,?)';
-                 let fields = [username, hash];
-                 let query = connection.query(sql, fields, function(e, results) {
-                     if (e) {
-                       throw e;
-                     }
-                       else {
-                         console.log("User Added to Database");
-                         res.redirect("/");
-                       }
-                   });
-               });
-          }
-       }
-    });
+  connection.query(sql, username, function(e, results) {
+    if (e) {
+      throw e;
+    } else {
+      if (results.length > 0) {
+        console.log("Username Already Exists");
+        res.redirect("signup");
+      } else {
+        bcrypt.hash(req.body.password, 10, function(e, hash) {
+          let sql = 'INSERT INTO users (username,password) VALUES (?,?)';
+          let fields = [username, hash];
+          let query = connection.query(sql, fields, function(e, results) {
+            if (e) {
+              throw e;
+            } else {
+              console.log("User Added to Database");
+              res.redirect("/");
+            }
+          });
+        });
+      }
+    }
+  });
 }
 
-app.listen(8081, console.log("Listening.."));
+app.listen(8080,  console.log("Listening.."));
