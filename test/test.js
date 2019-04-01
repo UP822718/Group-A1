203efe548e6d42b26ee6efb920cc03eb8388c572
William <up822718@myport.ac.uk>
2019-04-02 00:01:20 +0100

    make test better

--- test/test.js
+++ test/test.js
@@ -2,12 +2,12 @@
 const assert = require('assert');
 const puppeteer = require('puppeteer');
 
-  it('website is up', async function() {
-      const browser = await puppeteer.launch();
-      const page = await browser.newPage();
-      await page.goto('http://34.76.102.144:8080');
-      await browser.close();
-  });
+it('website is up', async function() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('http://34.76.102.144:8080');
+  await browser.close();
+});
 
 
 describe('Web interface', function() {
@@ -15,145 +15,187 @@ describe('Web interface', function() {
   describe('signup', function() {
     describe('invalid', function() {
       it("send back nothing", async function() {
-        const browser = await puppeteer.launch({headless: false});
+        const browser = await puppeteer.launch({
+          headless: false
+        });
         const page = await browser.newPage();
         await page.goto('http://34.76.102.144:8080/signup.html');
         output = await page.evaluate(() => {
           try {
-           document.documentElement.querySelector('#submit').click()
-           return false;
+            document.documentElement.querySelector('#submit').click()
+            return false;
           } catch (err) {
             return true;
           }
         });
         await browser.close();
-        assert.equal(true,output);
+        assert.equal(true, output);
       });
       it("send back username only", async function() {
-        const browser = await puppeteer.launch({headless: false});
+        const browser = await puppeteer.launch({
+          headless: false
+        });
         const page = await browser.newPage();
-        await page.goto('http://34.76.102.144:8080/signup.html', { waitUntil : ['load', 'domcontentloaded']});
+        await page.goto('http://34.76.102.144:8080/signup.html', {
+          waitUntil: ['load', 'domcontentloaded']
+        });
         output = await page.evaluate(() => {
           try {
             document.documentElement.querySelector("#particles-js > span > form > span:nth-child(1) > input[type='text']").setAttribute('value', 'value');
             document.documentElement.querySelector('#submit').click()
-           return false;
+            return false;
           } catch (err) {
             return true;
           }
         });
         await browser.close();
-        assert.equal(true,output);
+        assert.equal(true, output);
       });
 
       it("send back password only", async function() {
-        const browser = await puppeteer.launch({headless: false});
+        const browser = await puppeteer.launch({
+          headless: false
+        });
         const page = await browser.newPage();
-        await page.goto('http://34.76.102.144:8080/signup.html', { waitUntil : ['load', 'domcontentloaded']});
+        await page.goto('http://34.76.102.144:8080/signup.html', {
+          waitUntil: ['load', 'domcontentloaded']
+        });
         output = await page.evaluate(() => {
           try {
             document.documentElement.querySelector('#particles-js > span > form > span:nth-child(2) > input[type="password"]').setAttribute('value', 'value');
             document.documentElement.querySelector('#submit').click()
-           return false;
+            return false;
           } catch (err) {
             return true;
           }
         });
         console.log(output);
         await browser.close();
-        assert.equal(true,output);
+        assert.equal(true, output);
       });
       it("send back confirm confirm Password only", async function() {
-        const browser = await puppeteer.launch({headless: false});
+        const browser = await puppeteer.launch({
+          headless: false
+        });
         const page = await browser.newPage();
-        await page.goto('http://34.76.102.144:8080/signup.html', { waitUntil : ['load', 'domcontentloaded']});
+        await page.goto('http://34.76.102.144:8080/signup.html', {
+          waitUntil: ['load', 'domcontentloaded']
+        });
         output = await page.evaluate(() => {
           try {
             document.documentElement.querySelector('#particles-js > span > form > span:nth-child(3) > input[type="password"]').setAttribute('value', 'value');
             document.documentElement.querySelector('#submit').click()
-           return false;
+            return false;
           } catch (err) {
             return true;
           }
         });
         await browser.close();
-        assert.equal(true,output);
+        assert.equal(true, output);
       });
       it("send back confirm confirm Password and Password  only", async function() {
-        const browser = await puppeteer.launch({headless: false});
+        const browser = await puppeteer.launch({
+          headless: false
+        });
         const page = await browser.newPage();
-        await page.goto('http://34.76.102.144:8080/signup.html', { waitUntil : ['load', 'domcontentloaded']});
+        await page.goto('http://34.76.102.144:8080/signup.html', {
+          waitUntil: ['load', 'domcontentloaded']
+        });
         output = await page.evaluate(() => {
           try {
             document.documentElement.querySelector('#particles-js > span > form > span:nth-child(3) > input[type="password"]').setAttribute('value', 'value');
-              document.documentElement.querySelector('#particles-js > span > form > span:nth-child(2) > input[type="password"]').setAttribute('value', 'value');
+            document.documentElement.querySelector('#particles-js > span > form > span:nth-child(2) > input[type="password"]').setAttribute('value', 'value');
             document.documentElement.querySelector('#submit').click()
-           return false;
+            return false;
           } catch (err) {
             return true;
           }
         });
         await browser.close();
-        assert.equal(true,output);
+        assert.equal(true, output);
       });
-      it("send back confirm confirm Password and username  only",async function() {
-        const browser = await puppeteer.launch({headless: false});
+      it("send back confirm confirm Password and username  only", async function() {
+        const browser = await puppeteer.launch({
+          headless: false
+        });
         const page = await browser.newPage();
-        await page.goto('http://34.76.102.144:8080/signup.html', { waitUntil : ['load', 'domcontentloaded']});
+        await page.goto('http://34.76.102.144:8080/signup.html', {
+          waitUntil: ['load', 'domcontentloaded']
+        });
         output = await page.evaluate(() => {
           try {
             document.documentElement.querySelector("#particles-js > span > form > span:nth-child(1) > input[type='text']").setAttribute('value', 'value');
-              document.documentElement.querySelector('#particles-js > span > form > span:nth-child(2) > input[type="password"]').setAttribute('value', 'value');
+            document.documentElement.querySelector('#particles-js > span > form > span:nth-child(2) > input[type="password"]').setAttribute('value', 'value');
             document.documentElement.querySelector('#submit').click()
-           return false;
+            return false;
           } catch (err) {
             return true;
           }
         });
         await browser.close();
-        assert.equal(true,output); //to fix
+        assert.equal(true, output); //to fix
       });
 
       it("send back confirm confirm confirm Password and username  only", async function() {
-        const browser = await puppeteer.launch({headless: false});
+        const browser = await puppeteer.launch({
+          headless: false
+        });
         const page = await browser.newPage();
-        await page.goto('http://34.76.102.144:8080/signup.html', { waitUntil : ['load', 'domcontentloaded']});
+        await page.goto('http://34.76.102.144:8080/signup.html', {
+          waitUntil: ['load', 'domcontentloaded']
+        });
         output = await page.evaluate(() => {
           try {
             document.documentElement.querySelector("#particles-js > span > form > span:nth-child(1) > input[type='text']").setAttribute('value', 'value');
-              document.documentElement.querySelector('#particles-js > span > form > span:nth-child(2) > input[type="password"]').setAttribute('value', 'value');
+            document.documentElement.querySelector('#particles-js > span > form > span:nth-child(2) > input[type="password"]').setAttribute('value', 'value');
             document.documentElement.querySelector('#submit').click()
-           return false;
+            return false;
           } catch (err) {
             return true;
           }
         });
         await browser.close();
-        assert.equal(true,output); //to fix
+        assert.equal(true, output); //to fix
       });
     });
     describe('valid', function() {
       it("send back password and username", async function() {
-        const browser = await puppeteer.launch({headless: false});
+        const browser = await puppeteer.launch({
+          headless: false
+        });
         const page = await browser.newPage();
-        await page.goto('http://34.76.102.144:8080/signup.html', { waitUntil : ['load', 'domcontentloaded']});
+        await page.goto('http://34.76.102.144:8080/signup.html', {
+          waitUntil: ['load', 'domcontentloaded']
+        });
         output = await page.evaluate(() => {
           try {
-            document.documentElement.querySelector("#particles-js > span > form > span:nth-child(1) > input[type='text']").setAttribute('value', 'value');
-              document.documentElement.querySelector('#particles-js > span > form > span:nth-child(2) > input[type="password"]').setAttribute('value', 'value');
-            document.documentElement.querySelector('#submit').click()
-           return false;
+            document.documentElement.querySelector("#particles-js > span > form > span:nth-child(1) > input[type='text']").setAttribute('value', 'cat');
+            document.documentElement.querySelector('#particles-js > span > form > span:nth-child(2) > input[type="password"]').setAttribute('value', 'cat');
+            document.documentElement.querySelector('#particles-js > span > form > span:nth-child(3) > input[type="password"]').setAttribute('value', 'cat');
+            sleep(2000);
+            return false;
           } catch (err) {
             return true;
           }
         });
-        await browser.close();
-        assert.equal(true,output); //to fix
+
+        await page.click('#particles-js > span > form > span:nth-child(4) > input[type=submit]')
+        await page.waitForNavigation();
+
+          console.log(page.url());
+        //await browser.close();
+        if(page.url() == "http://34.76.102.144:8080/")
+        {
+          sert.equal(true, output);
+        }
+        else {
+                  assert.equal(true, false);
+        }
       });
 
 
 
-      });
+    });
   });
   describe('Login', function() {
     describe('invalid', function() {
